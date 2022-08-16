be71b710c7eec6a26d1af94f0baf99bdeb2b0aac
injecti0n <9992685+injecti0n@users.noreply.github.com>
2022-08-16 19:30:39 +0200

    updated

--- app.js
+++ app.js
@@ -30,40 +30,48 @@ function GameOver(sonuc) {
 
     }, 20000)
 }
-
+let firstClick=true
 let check = startRandom();
+// remove element by one by slow motion
+
 petals.forEach((petal, index) => {
     petal.addEventListener('click', (e) => {
-
-        //   let start = startRandom();
-        if (!check) {
-            check = true
-            console.log("seviyor")
-            durum.innerHTML = "SEVİYOR 😍"
-            fal[index] = "seviyor"
+        if (index === 0 && firstClick) {
+            alert('Soldan sağa')
         } else {
-            check = false
-            console.log("sevmiyorr")
-            durum.innerHTML = "SEVMİYOR 😢"
-            fal[index] = "sevmiyor"
+            //   let start = startRandom();
+            firstClick=false
+            if (!check) {
+                check = true
+                console.log("seviyor")
+                durum.innerHTML = "SEVİYOR 😍"
+                fal[index] = "seviyor"
+            } else {
+                check = false
+                console.log("sevmiyorr")
+                durum.innerHTML = "SEVMİYOR 😢"
+                fal[index] = "sevmiyor"
+            }
+
+            if (fal[0] == 'seviyor') {
+                console.log('oley be')
+
+                GameOver(fal[0])
+            }
+            if (fal[0] == 'sevmiyor') {
+                console.log('yak bi cugara')
+                GameOver(fal[0])
+                var audio = document.getElementById("myaudio");
+                audio.currentTime = 0;
+                audio.play();
+                setTimeout(function () {
+                    audio.pause();
+                }, 20000)
+            }
+            e.target.remove()
         }
 
-        if (fal[0] == 'seviyor') {
-            console.log('oley be')
-
-            GameOver(fal[0])
-        }
-        if (fal[0] == 'sevmiyor') {
-            console.log('yak bi cugara')
-            GameOver(fal[0])
-            var audio = document.getElementById("myaudio");
-            audio.currentTime = 0;
-            audio.play();
-            setTimeout(function(){
-                audio.pause();
-            },20000)
-        }
-        e.target.remove()
+        // remove petal every one second automatically
 
     })
 
@@ -105,37 +113,37 @@ var color = ['#cc3673', '#273522', '#7c001b', '#3a0003', '#dbd000', '#824275', '
 
 
 
-for (i = 0; i < 100; i++) {
-    for (x = 0; x < myh.children.length; x++) {
+// for (i = 0; i < 100; i++) {
+//     for (x = 0; x < myh.children.length; x++) {
 
-        var mypub = document.createElement("div");
+//         var mypub = document.createElement("div");
 
-        var size = randVal(sizebox) + "px";
+//         var size = randVal(sizebox) + "px";
 
-        mypub.style.backgroundColor = randVal(color);
+//         mypub.style.backgroundColor = randVal(color);
 
-        mypub.style.borderColor = randVal(color);
+//         mypub.style.borderColor = randVal(color);
 
-        mypub.style.width = size;
+//         mypub.style.width = size;
 
-        mypub.style.height = size;
+//         mypub.style.height = size;
 
-        mypub.style.left = randVal(100) + "%";
+//         mypub.style.left = randVal(100) + "%";
 
-        mypub.style.top = randVal(100) + "%";
+//         mypub.style.top = randVal(100) + "%";
 
-        myh.children[x].appendChild(mypub);
+//         myh.children[x].appendChild(mypub);
 
-    }
-}
-var ct = document.getElementById("center");
+//     }
+// }
+// var ct = document.getElementById("center");
 
-setInterval(function () {
-    lV.style.backgroundColor = randVal(color);
-    lV.nextElementSibling.style.backgroundColor = randVal(color);
+// setInterval(function () {
+//     lV.style.backgroundColor = randVal(color);
+//     lV.nextElementSibling.style.backgroundColor = randVal(color);
 
-    for (i = 0; i < ct.children.length; i++) {
-        ct.children[i].style.backgroundColor = randVal(color);
-    }
+//     for (i = 0; i < ct.children.length; i++) {
+//         ct.children[i].style.backgroundColor = randVal(color);
+//     }
 
-}, 1000);
+// }, 1000);
